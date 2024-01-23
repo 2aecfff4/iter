c79213202247b7cdf2ad7b09ecdc5846ce57dbc1
2aecfff4 <11327470+2aecfff4@users.noreply.github.com>
2024-01-23 15:03:03 +0100

    Add examples

--- examples.cpp
+++ examples.cpp
@@ -1,10 +1,11 @@
 #include "concepts.hpp"
+#include "enumerate.hpp"
 #include "filter.hpp"
 #include "generator.hpp"
 #include "iterator.hpp"
 #include "map.hpp"
 #include "vector.hpp"
-#include <iostream>
+#include <print>
 #include <vector>
 
 namespace examples {
@@ -15,11 +16,32 @@ constexpr auto generator() -> iter::iterator auto {
         .map([](const int& i) { return (float)i + 0.1f; });
 }
 
+constexpr auto enumerate() -> iter::iterator auto {
+    return iter::gen(0, 16) //
+        .enumerate()
+        .filter([](const auto& t) {
+            const auto& [idx, v] = t;
+            return (v % 2) == 0;
+        });
+}
+
 } // namespace examples
 
 int main() {
     {
-        auto g = examples::generator();
-        //
+        std::println("--examples::generator--");
+        auto i = examples::generator();
+        while (auto v = i.next()) {
+            std::println("{}", *v);
+        }
+    }
+
+    {
+        std::println("--examples::enumerate--");
+        auto i = examples::enumerate();
+        while (auto t = i.next()) {
+            const auto& [idx, v] = *t;
+            std::println("{}: {}", idx, v);
+        }
     }
 }
